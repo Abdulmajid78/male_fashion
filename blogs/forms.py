f45f4b5f711c2a5b55271f46e3d7fde0e14948d8
Abdumajid7 <jackodilov@gmail.com>
2023-02-15 00:18:50 +0500

    comment section ended

--- blogs/forms.py
+++ blogs/forms.py
@@ -1,2 +1,14 @@
 from django import forms
+from .models import CommentModel
 
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = CommentModel
+        exclude = ['blog', 'created_at']
+        widgets = {
+            'name': forms.TextInput(attrs={'placeholder': 'name'}),
+            'email': forms.EmailInput(attrs={'placeholder': 'email'}),
+            'phone': forms.TextInput(attrs={'placeholder': 'phone'}),
+            'comment': forms.Textarea(attrs={'placeholder': 'comment'}),
+        }
--- blogs/views.py
+++ blogs/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, reverse, redirect, get_object_or_404
 from .models import BlogModel
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView
+from .forms import CommentForm
 
 
 class BlogListView(ListView):
@@ -11,3 +12,22 @@ class BlogListView(ListView):
 class BlogDetailView(DetailView):
     template_name = 'main/blog-details.html'
     model = BlogModel
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['comment_form'] = CommentForm()
+        return context
+
+
+class CommentCreateView(CreateView):
+    template_name = 'main/blog-details.html'
+    form_class = CommentForm
+
+    def form_valid(self, form):
+        blog = get_object_or_404(BlogModel, id=self.kwargs.get('pk'))
+        form.instance.blog = blog
+        form.instance.save()
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse('blogs:detail', kwargs={'pk': self.kwargs.get('pk')})
